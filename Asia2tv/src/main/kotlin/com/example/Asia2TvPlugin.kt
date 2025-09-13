e719eccd74088ca5720a122071a091ef42cffcb7
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-13 17:47:08 +0100

    Update Asia2TvPlugin.kt

--- Asia2tv/src/main/kotlin/com/example/Asia2TvPlugin.kt
+++ Asia2tv/src/main/kotlin/com/example/Asia2TvPlugin.kt
@@ -1,16 +1,13 @@
-package com.example
+package com.wolker.asia2tv
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
-// This is the new entry point for your plugin.
-// CloudStream scans for this annotation to discover the plugin.
 @CloudstreamPlugin
 class Asia2TvPlugin: Plugin() {
-    // This function is called when the app starts.
-    // Its job is to register your provider class.
     override fun load(context: Context) {
+        // لا حاجة لـ import لأن كلا الملفين في نفس الحزمة الآن
         registerMainAPI(Asia2Tv())
     }
 }
