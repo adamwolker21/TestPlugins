6fe4972c04ede0571de1f792546915be327249d9
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-10-03 01:24:51 +0100

    Update WeCimaPlugin.kt

--- WeCima/src/main/kotlin/com/example/WeCimaPlugin.kt
+++ WeCima/src/main/kotlin/com/example/WeCimaPlugin.kt
@@ -3,11 +3,13 @@ package com.example
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.example.extractors.WeCimaExtractor
 
 @CloudstreamPlugin
 class WeCimaPlugin: Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner.
         registerMainAPI(WeCimaProvider())
+        // v16: Register the new extractor
+        registerExtractorAPI(WeCimaExtractor())
     }
 }
