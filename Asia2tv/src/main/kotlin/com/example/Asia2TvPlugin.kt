56e74ce19073a0055819917c9539a868c7a9a99d
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-10-15 15:02:56 +0100

    Update Asia2TvPlugin.kt

--- Asia2tv/src/main/kotlin/com/example/Asia2TvPlugin.kt
+++ Asia2tv/src/main/kotlin/com/example/Asia2TvPlugin.kt
@@ -1,4 +1,4 @@
-package com.wolker.asia2tv
+package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -7,7 +7,8 @@ import android.content.Context
 @CloudstreamPlugin
 class Asia2TvPlugin: Plugin() {
     override fun load(context: Context) {
-        // لا حاجة لـ import لأن كلا الملفين في نفس الحزمة الآن
+        // All providers should be added in this manner.
+        // Please don't edit the names.
         registerMainAPI(Asia2Tv())
     }
 }
